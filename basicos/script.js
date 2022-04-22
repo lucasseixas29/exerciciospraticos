1ab46a79b832c33d65819bb9f30e4ac00e11314f
Lucas Seixas <lucasseixas29@hotmail.com>
2022-04-22 17:53:41 -0300

    Treinando if e else

---
+++ basicos/script.js
@@ -0,0 +1,2 @@
+/*var a = 22
+console.log(a)*/
---
+++ basicos/signos.js
@@ -0,0 +1,29 @@
+var mês = prompt(`Digite o mês que você nasceu, para descobrir o seu signo.`)
+
+if(mês == 'Março') {
+  alert('Áries')
+} else if(mês == 'Abril') {
+    alert('Touro')
+} else if(mês == 'Maio') {
+    alert('Gêmeos')
+} else if(mês == 'Junho') {
+    alert('Câncer')
+} else if(mês == 'Julho') {
+    alert('Leão')
+} else if(mês == 'Agosto') {
+    alert('Virgem') 
+} else if(mês == 'Setembro') {
+    alert('Libra') 
+} else if(mês == 'Outubro') {
+    alert('Escorpião') 
+} else if(mês == 'Novembro') {
+    alert('Sagitário')
+} else if(mês == 'Dezembro') {
+    alert('Capricórnio')
+} else if(mês == 'Janeiro') {
+    alert('Aquário')
+} else if(mês == 'Fevereiro') {
+    alert('Peixes')
+} else {
+    alert('DIGITE UM MÊS')
+}
